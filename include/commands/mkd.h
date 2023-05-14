462fd90ae7e7200033021cd09a40f9af25039ff0
SvetlanaGo <SvetlanaG_07@mail.ru>
2023-05-14 11:07:05 +0300

    Implement MKD command.

--- include/commands/mkd.h
+++ include/commands/mkd.h
@@ -3,4 +3,4 @@
 #include "user.h"
 #include "utils.h"
 
-void run_mkd(const struct user* const current_user, char* const argument);
+void run_mkd(struct user* const current_user, char* const argument);
--- src/commands/mkd.c
+++ src/commands/mkd.c
@@ -1,20 +1,27 @@
 #include "commands/mkd.h"
 
-void run_mkd(const struct user* const current_user, char* const argument) {
+void run_mkd(struct user* const current_user, char* const argument) {
 	if (!current_user->authenticated) {
         send_response(current_user->control_socket, "530 Not logged in.\r\n");
         return;
     }
-    if (!argument) {}
 	
-    /*char response[BUFFER_SIZE];
-    char directory[BUFFER_SIZE];
+    if (!argument) {
+		send_response(current_user->control_socket, "550 Invalid path\r\n");
+        return;
+	}
+	
+    char response[BUFFER_SIZE];
+    char directory[PARAM_SIZE];
+	const size_t response_length = BUFFER_SIZE;
     get_absolute_path(argument, directory, current_user->current_directory);
+	pthread_mutex_lock(&fs_mutex);
     if (mkdir(directory, 0777) == 0) {
-        snprintf(response, BUFFER_SIZE, "257 \"%s\" created\r\n", argument);
+        snprintf(response, response_length, "257 \"%s\" created\r\n", argument);
     } else {
-        snprintf(response, BUFFER_SIZE, "550 %s\r\n", strerror(errno));
+        snprintf(response, response_length, "550 %s\r\n", strerror(errno));
     }
+	
+	pthread_mutex_unlock(&fs_mutex);
     send_response(current_user->control_socket, response);
-    */
 }
